294035b2e38de170af0c40845829a47839e601ee
Szabó János <netbuilder1973@gmail.com>
2025-03-24 00:39:39 +0100

    Update vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -10,9 +10,9 @@ module.exports = defineConfig({
         __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: JSON.stringify(true)
       }),
       new webpack.EnvironmentPlugin({
-        "CLOUDINARY_CLOUD_NAME": "ds9w2ccsn",
-        "CLOUDINARY_API_KEY": "463942451693814",
-        "CLOUDINARY_API_SECRET": "tLMxL0JDBX38pPdqx0o1Xan1WdI"
+        "CLOUDINARY_CLOUD_NAME": "",
+        "CLOUDINARY_API_KEY": "",
+        "CLOUDINARY_API_SECRET": ""
         // "CLOUDINARY_CLOUD_NAME": JSON.stringify(process.env.CLOUDINARY_CLOUD_NAME),
         // "CLOUDINARY_API_KEY": JSON.stringify(process.env.CLOUDINARY_API_KEY),
         // "CLOUDINARY_API_SECRET": JSON.stringify(process.env.CLOUDINARY_API_SECRET)
